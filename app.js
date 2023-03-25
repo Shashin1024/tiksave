f502021671c66c98b78611e50ca5acc8d7e145e1
Shashin1024 <42843764+Shashin1024@users.noreply.github.com>
2023-03-25 13:37:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,37 +4,30 @@ const tiktok = require('.');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000
+const port = 3000;
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: "http://localhost:80",
-}))
+  origin: "http://localhost:80",
+}));
 
 app.post('/tiktok', (req, res) => {
-  const url = (req.body.urll).toString();
+  const url = (req.body.url).toString();
   const slider_url = 'https://vt.tiktok.com/ZS8e1HeEs/';
-  
-  tiktok.tiklydown(url)
-    .then(result => {
-       //res.json(url);
-        res.json(result);
-    })
-    .catch(e => {
-      console.log(e);
-      res.status(500).send('An error occurred');
-    });
 
-  tiktok.dlpanda(slider_url)
-    .then(result => {
-      res.json(result);
+  Promise.all([
+    tiktok.tiklydown(url),
+    tiktok.dlpanda(slider_url),
+  ])
+    .then(([tiklydownResult, dlpandaResult]) => {
+      res.json({ tiklydownResult, dlpandaResult });
     })
-    .catch(e => {
+    .catch((e) => {
       console.log(e);
       res.status(500).send('An error occurred');
     });
 });
 
 app.listen(process.env.port || port, () => {
-  console.log('Server listening on port ${port}');
+  console.log(`Server listening on port ${port}`);
 });
