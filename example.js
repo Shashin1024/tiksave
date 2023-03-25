b28b482b2bde17ed2f49585b6b8bd2a3e5076b64
Shashin1024 <42843764+Shashin1024@users.noreply.github.com>
2023-03-25 21:48:41 +0530

    Add files via upload

--- example.js
+++ example.js
@@ -1,15 +1,33 @@
-const tiktok = require('.')
-const url = 'https://www.tiktok.com/@__kaviii.girl__/video/7195959183362116891?is_from_webapp=1&sender_device=pc'
-const slider_url = 'https://vt.tiktok.com/ZS8e1HeEs/'
+const express = require('express');
+const tiktok = require('.');
+const app = express();
+const port = 3000;
 
-tiktok.tiklydown(url)
-     .then(result => {
-          console.log(result)
-     })
-     .catch(e => console.log(e))
+app.get('/', function(req, res) {
+  const url = 'https://www.tiktok.com/@__kaviii.girl__/video/7195959183362116891?is_from_webapp=1&sender_device=pc';
+  const slider_url = 'https://vt.tiktok.com/ZS8e1HeEs/';
+  
+  tiktok.tiklydown(url)
+    .then(result => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    });
 
-tiktok.dlpanda(slider_url)
-     .then(result => {
-          console.log(result)
-     })
-     .catch(e => console.log(e))
+  tiktok.dlpanda(slider_url)
+    .then(result => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
