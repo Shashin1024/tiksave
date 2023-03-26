9b60a4babc5081b5c8342a481935e49e3594b387
Shashin1024 <42843764+Shashin1024@users.noreply.github.com>
2023-03-26 17:21:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 
 app.get('/tiktokk', (req, res) => {
-    const url = (req.body.url).toString();
+    const url = "https://www.tiktok.com/@__kaviii.girl__/video/7195959183362116891?is_from_webapp=1&sender_device=pc";
     const slider_url = 'https://vt.tiktok.com/ZS8e1HeEs/';
   
     Promise.all([
@@ -30,7 +30,22 @@ app.get('/tiktokk', (req, res) => {
   });
 
 
+app.post('/tiktok', (req, res) => {
+  const url = (req.body.url).toString();
+  const slider_url = 'https://vt.tiktok.com/ZS8e1HeEs/';
 
+  Promise.all([
+    tiktok.tiklydown(url),
+    tiktok.dlpanda(slider_url),
+  ])
+    .then(([tiklydownResult, dlpandaResult]) => {
+      res.json({ tiklydownResult, dlpandaResult });
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).send('An error occurred');
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
