87fdd3d77fcdf09dce89e444ba3a4a3aad293054
Shashin1024 <42843764+Shashin1024@users.noreply.github.com>
2023-03-26 17:19:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,23 +11,26 @@ app.use(cors({
   origin: "*",
 }));
 
-app.post('/tiktok', (req, res) => {
-  const url = "https://www.tiktok.com/@__kaviii.girl__/video/7195959183362116891?is_from_webapp=1&sender_device=pc";
-  const slider_url = 'https://vt.tiktok.com/ZS8e1HeEs/';
+app.get('/tiktokk', (req, res) => {
+    const url = (req.body.url).toString();
+    const slider_url = 'https://vt.tiktok.com/ZS8e1HeEs/';
+  
+    Promise.all([
+      tiktok.tiklydown(url),
+      tiktok.dlpanda(slider_url),
+    ])
+      .then(([tiklydownResult, dlpandaResult]) => {
+        res.json({ tiklydownResult, dlpandaResult });
+        console.log({ tiklydownResult, dlpandaResult });
+      })
+      .catch((e) => {
+        console.log(e);
+        res.status(500).send('An error occurred');
+      });
+  });
 
-  Promise.all([
-    tiktok.tiklydown(url),
-    tiktok.dlpanda(slider_url),
-  ])
-    .then(([tiklydownResult, dlpandaResult]) => {
-      res.json({ tiklydownResult, dlpandaResult });
-      console.log({ tiklydownResult, dlpandaResult });
-    })
-    .catch((e) => {
-      console.log(e);
-      res.status(500).send('An error occurred');
-    });
-});
+
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
