a5e27ddc7d7ac26edb327250c145145437dc6ca9
Shashin1024 <42843764+Shashin1024@users.noreply.github.com>
2023-03-26 17:00:11 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const tiktok = require('.');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(cors({
